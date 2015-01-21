1f1b5ba59ae17ea43608e1409605eab3a1146282
Lori Zanotta <lori.zanotta@gmail.com>
2015-01-21 10:00:17 -0800

    add routes

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,3 +1,27 @@
-(function() {
 
-});
+// var body = document.createElement('body');
+// var list = document.getElementsByTagName('h1')[0];
+
+// var title = document.createElement('h1');
+// var text1 = document.createTextNode('My JavaScript Blog');
+// title.appendChild(text1);
+// var list2 = document.getElementsByTagName('body')[0];
+// list2.insertBefore(title, list2.childNodes[0]);
+
+
+
+// var heading = document.createElement('p');
+// var text = document.createTextNode(" My Second Blog Post");
+// heading.appendChild(text);
+// document.getElementsByTagName('h1')[0].appendChild(heading);
+
+
+// //list1.insertBefore(heading, list1.childNodes[0]);
+
+// var para = document.createElement('p');
+// var text2 = document.createTextNode("This is some really awesome stuff.");
+// para.appendChild(text2);
+//  var list3 = document.getElementsByTagName('h1')[1];
+//  list3.insertBefore(para, list3.nextSibling);
+
+
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'My Blog!' });
 });
 
+/* GET blog1 page. */
+router.get('/', function(req, res) {
+  res.render('blog1');
+});
+
+/* GET blog2 page. */
+router.get('/', function(req, res) {
+  res.render('blog2');
+});
+
+/* GET blog3 page. */
+router.get('/', function(req, res) {
+  res.render('blog3');
+});
+
 module.exports = router;
